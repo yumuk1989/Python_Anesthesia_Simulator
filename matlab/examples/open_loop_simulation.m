f15e612c86b9c8a698c54dc1e0c4b9375041a29c
Michele Schiavo <michele.schiavo@unibs.it>
2025-05-27 11:41:18 +0200

    Exmples: improved the documentation of the open_loop_simulation.m script

--- matlab/examples/open_loop_simulation.m
+++ matlab/examples/open_loop_simulation.m
@@ -2,101 +2,122 @@ clear all
 close all
 clc
 
-%% Set Python environment
-% Change with your path
+%% Environment Setup
+% Configures MATLAB to use a specific Python environment
 
-% Windows
+% For Windows users
+% Replace 'your_path\your_environment\Scripts\python.exe' with the actual
+% path on your machine.
+
 env = pyenv('Version', ...
     'your_path\your_environment\Scripts\python.exe');
 
-% Linux/macOS
+
+% For Linux/macOS users
+% Replace '/Users/your_username/your_environment/bin/python' with the
+% actual path on your machine.
+
 % env = pyenv('Version', ...
 %    '/Users/your_username/your_environment/bin/python');
 
 %% Instantiate a Patient Object
-% Import the simulator module
+
+% Imports the Python module containing the simulator.
 simulator =...
     py.importlib.import_module('python_anesthesia_simulator.simulator');
 
 % Define patient parameters
-age = 18;       % years
-height = 170;   % cm
-weight = 60;    % kg
-gender = 0;     % 0 = female, 1 = male
-Ts = 1;         % s
+age = 18;                   % years
+height = 170;               % cm
+weight = 60;                % kg
+gender = 0;                 % 0 = female, 1 = male
+sampling_time = 1;          % seconds
 
-% Instantiate a Patient object
-George = simulator.Patient([age, height, weight, gender], ts = Ts);
+% Creates a patient object with specified parameters
+George = simulator.Patient([age, height, weight, gender],...
+    ts = sampling_time);
 
-%% Simulation
+%% Simulation setup
 
-T_simu = 3600;             % total simulation time in seconds
-N_simu = floor(T_simu/Ts); % total number of simulation steps
+% Defines the total simulation time and calculates the number of simulation
+% steps according to the sapling time
+T_simu = 3600;                        % total simulation time in seconds
+N_simu = floor(T_simu/sampling_time); % total number of simulation steps
 
-% Initialize infusion profiles
+
+% Initialize infusion profiles for propofol and remifentanil
 propofol_infusion_profile = zeros(N_simu,1);        % mg/s
 remifentanil_infusion_profile = zeros(N_simu,1);    % ug/s
 
 % Propofol profile
-propofol_infusion_profile(1:floor(50/Ts)) = ...
+propofol_infusion_profile(1:floor(50/sampling_time)) = ...
     2;           % 2 mg/s for 50 seconds
-propofol_infusion_profile(floor(121/Ts):end) = ...
+propofol_infusion_profile(floor(121/sampling_time):end) = ...
     0.2;      % 0.2 mg/s from 121s onward
 
 % Remifentanil profile
-remifentanil_infusion_profile(1:floor(30/Ts)) = ...
+remifentanil_infusion_profile(1:floor(30/sampling_time)) = ...
     1;       % 1 ug/s for 30 seconds
-remifentanil_infusion_profile(floor(121/Ts):end) = ...
+remifentanil_infusion_profile(floor(121/sampling_time):end) = ...
     0.1;  % 0.1 ug/s from 121s onward
 
+%% Run the simulation
 
+% Executes the simulation
 df = George.full_sim(u_propo = propofol_infusion_profile,...
     u_remi = remifentanil_infusion_profile);
 
-% Convert Python DataFrame to MATLAB table
+% Convert the Python DataFrame to a MATLAB table
 George_tbl = table(df);
 
-%% Plot the results
-Time = George_tbl.Time;
-% Plot control inputs
+%% Plotting results
+
+% Effect site concentrations and infusion rates
 figure
 subplot(2,1,1)
-plot(Time, George_tbl.x_propo_4, 'DisplayName', 'Propofol [ug/ml]') 
+plot(George_tbl.Time, George_tbl.x_propo_4,...
+    'DisplayName', 'Propofol [ug/ml]') 
 hold on
-plot(Time, George_tbl.x_remi_4, 'DisplayName', 'Remifentanil [ng/ml]')
+plot(George_tbl.Time, George_tbl.x_remi_4,...
+    'DisplayName', 'Remifentanil [ng/ml]')
 xlabel('Time [s]')
 ylabel('Effect Site Concentration')
 legend('Location','southeast')
 grid on
+title('Effect site concentrations and infusion rates')
 
 subplot(2,1,2)
-plot(Time, George_tbl.u_propo, 'DisplayName', 'Propofol [mg/s]')
+plot(George_tbl.Time, George_tbl.u_propo,...
+    'DisplayName', 'Propofol [mg/s]')
 hold on
-plot(Time, George_tbl.u_remi, 'DisplayName', 'Remifentanil [ug/s]')
+plot(George_tbl.Time, George_tbl.u_remi,...
+    'DisplayName', 'Remifentanil [ug/s]')
 ylabel('Infusion Rate')
 legend('Location','northeast')
 grid on
 
 
-% Plot physiological responses
+
+% Physiological responses
 figure
 subplot(4,1,1)
-plot(Time, George_tbl.BIS)
+plot(George_tbl.Time, George_tbl.BIS)
 ylabel('BIS')
 grid on
+title('Physiological responses')
 
 subplot(4,1,2)
-plot(Time, George_tbl.MAP)
+plot(George_tbl.Time, George_tbl.MAP)
 ylabel('MAP')
 grid on
 
 subplot(4,1,3)
-plot(Time, George_tbl.CO)
+plot(George_tbl.Time, George_tbl.CO)
 ylabel('CO')
 grid on
 
 subplot(4,1,4)
-plot(Time, George_tbl.TOL)
+plot(George_tbl.Time, George_tbl.TOL)
 ylabel('TOL')
 grid on
 xlabel('Time [s]')
