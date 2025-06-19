856197252387de9e3631aad482e7698c8908504a
Erhan Yumuk <erhanyumuk@gmail.com>
2025-06-19 10:43:51 +0300

    Add realistic2 disturbance profile to feature/ugent_v2

--- src/python_anesthesia_simulator/disturbances.py
+++ src/python_anesthesia_simulator/disturbances.py
@@ -11,7 +11,7 @@ def compute_disturbances(time: float, dist_profil: str = 'realistic',
     time : float
         Time: in seconds.
     dist_profil : str, optional
-        disturbance profile, can be: 'realistic', 'simple', 'step' or "null". The default is 'realistic'.
+        disturbance profile, can be: 'realistic', 'realistic2', 'liver_transplantation', 'simple', 'step' or "null". The default is 'realistic'.
     start_step : float, optional
         start time of the step distuebance (seconds). The default is 600s.
     end_step : float, optional
@@ -55,6 +55,39 @@ def compute_disturbances(time: float, dist_profil: str = 'realistic',
                                   [51,   10,  5, 0.2],
                                   [56,   10,  5, 0.2],
                                   [56.5,  0,  0, 0]])
+        
+    elif dist_profil == 'realistic2':
+         # As proposed in Ionescu, Clara M., et al. "An open source patient simulator for design and evaluation of computer 
+         # based multiple drug dosing control for anesthetic and hemodynamic variables." IEEE Access 9 (2021): 8680-8694.
+         # doi: 10.1109/ACCESS.2021.3049880
+
+        Disturb_point = np.array([[0,     0,  0, 0],  # time, BIS signal, MAP, CO signals
+                                  [9.9,   0,  0, 0],
+                                  [10,   20, 10, 0.5],
+                                  [15,   20, 10, 0.5],
+                                  [15.1,  0,  0, 0],
+                                  [19.9,  0,  0, 0],
+                                  [20,   20, 10, 0.5],
+                                  [25,   20, 10, 0.5],
+                                  [25.1,  0,  0, 0],
+                                  [26.9,-20,-10, -0.5],
+                                  [27,   20, 10, 0.5],
+                                  [32,   20, 10, 0.5],
+                                  [32.1,  0,  0, 0],
+                                  [41.9,  0,  0, 0],
+                                  [42,   20, 10, 0.5],
+                                  [44,   20, 10, 0.5],
+                                  [44.1,  0,  0, 0],
+                                  [50,    0,  0, 0],
+                                  [50.1, 20, 10, 0.5],
+                                  [55,   20, 10, 0.5],
+                                  [55.1,  0,  0, 0],
+                                  [75,    0,  0, 0],
+                                  [75.1, 20, 10, 0.5],
+                                  [95,   20, 10, 0.5],
+                                  [95.1,  0,  0, 0],
+                                  [100,   0,  0, 0]])
+        
     elif dist_profil == 'simple':
         # As in G. A. Dumont, A. Martinez, and J. M. Ansermino,
         # “Robust control of depth of anesthesia,”
